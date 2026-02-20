8b5c86db7175203e989bec891a6fbc60d4ac8389
Dylan Emerson <emersonds2868@gmail.com>
2026-02-20 07:26:12 -0500

    Added user input
    Scanner does not read text correctly, currently only creates the task item without a name

--- todo-list/main.go
+++ todo-list/main.go
@@ -1,23 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type ToDo struct {
 	IsComplete bool
 	Name       string
-	//ChangeCompletionStatus(bool)
 }
 
+// Marks a task complete or incomplete depending on completeStatus
 func (t *ToDo) ChangeCompletion(completeStatus bool) {
 	t.IsComplete = completeStatus
 }
 
 func main() {
-	todo1 := ToDo{IsComplete: false, Name: "Learn Go!"}
+	/*
+		todo1 := ToDo{IsComplete: false, Name: "Learn Go!"}
 
-	fmt.Println("TODO:", todo1.Name, "Completed?", todo1.IsComplete)
+		fmt.Println("TODO:", todo1.Name, "Completed?", todo1.IsComplete)
 
-	todo1.ChangeCompletion(true)
+		todo1.ChangeCompletion(true)
 
-	fmt.Println("TODO:", todo1.Name, "Completed?", todo1.IsComplete)
+		fmt.Println("TODO:", todo1.Name, "Completed?", todo1.IsComplete)
+	*/
+	tasks := make([]ToDo, 10)
+
+	// Used to get user input with spaces
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Create a task to add to your todo list...")
+	scanner.Scan()
+	input := scanner.Text()
+	tasks = append(tasks, ToDo{IsComplete: false, Name: input})
+
+	fmt.Println("Task:", tasks[0].Name, "Completed:", tasks[0].IsComplete)
 }
